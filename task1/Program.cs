6fe92f90c720d4c6d0862c7bc07c7de47afad3e5
olga_avdukova <aos-83@ya.ru>
2023-05-17 18:58:13 +0300

    Ввод условий задачи и данных в консоль

---
+++ task1/Program.cs
@@ -0,0 +1,33 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк,
+//длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры,
+// либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями,
+//  лучше обойтись исключительно массивами.
+
+string Prompt(string message) {
+    System.Console.Write(message);
+    string input = Console.ReadLine();
+    return input;
+}
+void ShowArray(string[] array) {
+    for (int i = 0; i < array.Length; i++) {
+        System.Console.Write($"{array[i]}  ");
+    }
+    System.Console.WriteLine();
+}
+string[] CreateArray() {   
+    int count = 0;
+    string value = String.Empty;
+    string[] secondArray = new string[50];
+    System.Console.WriteLine($"Введите 'exit' для завершения ввода");
+    for (count = 0; value != "exit"; count++) {
+        value = Prompt($"Введите {count+1}-ое значение ");
+        if (value != "exit") {
+            secondArray[count] = value;
+        }
+    }
+    string[] array = new string[count-1];
+    for (int i = 0; i < array.Length; i++) {
+         array[i] = secondArray[i];
+    }
+    return array;
+}
